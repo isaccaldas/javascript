a10d8ac10bb9e8874cf7ff1e09bffbfde4b67a31
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-01-11 08:15:21 -0300

    concatening

---
+++ constructing-strings-with-variables.js
@@ -0,0 +1,14 @@
+/*
+Constructing Strings with Variables
+Sometimes you will need to build a string, Mad Libs style. By using the concatenation operator (+), you can insert one or more variables into a string you're building.
+
+Example:
+
+const ourName = "freeCodeCamp";
+const ourStr = "Hello, our name is " + ourName + ", how are you?";
+ourStr would have a value of the string Hello, our name is freeCodeCamp, how are you?.
+*/
+
+// Only change code below this line
+const myName = "Isac";
+const myStr = "My name is " + myName + " I am well!";
