0c85edc3b3d72830ac1d95d0a8197e90cb5b9bcc
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-01-11 11:28:14 -0300

    appending e legth

---
+++ appending-variables-to-strings.js
@@ -0,0 +1,17 @@
+/*
+Appending Variables to Strings
+Just as we can build a string over multiple lines out of string literals, we can also append variables to a string using the plus equals (+=) operator.
+
+Example:
+
+const anAdjective = "awesome!";
+let ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+ourStr would have the value freeCodeCamp is awesome!.
+*/
+
+// Change code below this line
+const someAdjective = "Hello";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
