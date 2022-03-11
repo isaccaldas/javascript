33e509bbc6247d89c3c907a2b962f3116a64116a
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-03-11 10:20:58 -0300

    variáveis

---
+++ dataTypes.js
@@ -0,0 +1,18 @@
+//Possui 3 tipos de dados Number, boolean e String
+
+//Numeros
+10-5; // subtraindo números
+12345 + 56789; //adicionando números
+22 + 33 + 44; // Adicionando múltiplos números
+123 * 456; // Multiplicando números
+1234 + 57 * 3 - 31 / 4; // combinando todas as operações
+
+
+//Strings
+"ola";
+
+//booleano
+true;
+
+
+
---
+++ variaveisExemplo.js
@@ -0,0 +1,32 @@
+var nome; //Declaração de uma variável
+//undefined é o que javascript faz quando não retorna um valor.
+
+nome = "Pedro"; //Inicializando uma variável
+
+var outroNome = "Maria"; //Declarando e inicializando uma variável na mesma linha.
+
+outroNome; // Retorna o valor armazenado, nesse caso "Maria"
+
+outroNome = "Carlos"; //Atribuindo um novo valor a variável
+
+var numeroDoces = 8;
+var numeroIrmaos = 1 + 3;
+var resultado = numeroDoces/ numeroIrmaos;
+
+//Incremento e Decremento
+var numero = 10;
+numero++; //retorna o resultado da variável e depois incrementa em um. Valor 10 
+numero;   //retorna o resultado da variável. Valor 11
+++numero; //incrementa em o valor em um e depois retorna o resultado da variável. Valor 12
+numero;   //retorna o resultado da variável. Valor 12
+--numero; //decrementa em o valor em um e depois retorna o resultado da variável. Valor 11
+numero;   //retorna o resultado da variável. Valor 11
+
+// aumentar o valor da variável com uma certa quantidade.
+var x = 10;
+x = x + 5; //adicione 5 a x;
+x; // passa a valer 15
+x += 5;  //outra forma de adicionar 5 a x;
+x; // passa a valer 20
+x -= 10;  //outra forma de subtrair 10 de x;
+
