4f299a0b6307f5bce6495c65ae13215058dc2a9f
isaccaldas <caldas.isac@gmail.com>
2025-09-09 16:46:03 -0300

    comentarios

--- Treinamento/comentarios.js
+++ Treinamento/comentarios.js
@@ -15,5 +15,6 @@
 
 /* This is a
 multi-line comment */
+
 //NOTE: As you write code, you should regularly add comments to clarify the 
 //function of parts of your code. Good commenting can help communicate the intent of your code—both for others and for your future self.
